67f75d7a28c025a83e2ec53b596396e9b8d9a372
Marcela Silvério <marcelapsilverio@gmail.com>
2021-05-13 21:13:22 -0300

    Initial commit

---
+++ include/StateMan.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Engine{
+    class StateMan.hpp {
+    
+        public:
+            StateMan.hpp();
+            ~StateMan.hpp();
+};
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
+}
